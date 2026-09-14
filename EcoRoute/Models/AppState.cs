3084d20347f3c58112ab34763e6c5e1f282f6618
agaggigit <ma.argyabagaskara@gmail.com>
2026-09-14 10:35:57 +0700

    feat: add AppViewState and BaseViewModel with INotifyPropertyChanged

---
+++ EcoRoute/Models/AppState.cs
@@ -0,0 +1,11 @@
+namespace EcoRoute.Models;
+
+public enum AppState
+{
+    StateA_Idle,
+    StateB_SearchLocation,
+    StateC_InputRoute,
+    StateD_RouteResult,
+    StateE_Error,
+    StateL_Loading
+}
